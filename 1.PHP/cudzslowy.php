795dc651a07a929b7c2c7dd76802f826694da653
turtlehtml <p.kozlowski.1312@gmail.com>
2024-09-16 11:09:21 +0200

    bedzie cięte

---
+++ 1.PHP/cudzslowy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $poczatek= 'Ala ma';
+    $liczba=4;
+    $koniec= ' koty';
+    ?>
+    <?php echo '$poczatek $liczba $koniec a chce mieć jeszcze dwa.';?><br>
+    <?php echo "$poczatek $liczba $koniec a chce mieć jeszcze dwa.";?>
+</body>
+</html>
