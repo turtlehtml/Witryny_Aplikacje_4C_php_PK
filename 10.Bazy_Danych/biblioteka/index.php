58155d2cf0c9aed4f774f9b55269a80bfdf25840
turtlehtml <p.kozlowski.1312@gmail.com>
2025-04-14 11:13:21 +0200

    jej

--- 10.Bazy_Danych/biblioteka/index.php
+++ 10.Bazy_Danych/biblioteka/index.php
@@ -1,66 +1,92 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        *{
+        * {
             box-sizing: border-box;
         }
-        .column{
+
+        .column {
             float: left;
             padding: 10px;
-            
-        }
-        .left{
-            background-color: lightgreen;
-            width: 15%;
-            
 
         }
-        .right{
+
+        .left {
+            background-color: lightgreen;
+            width: 15%;
+
+
+        }
+
+        .right {
             background-color: greenyellow;
             width: 85%;
             height: auto;
             min-height: 150px;
         }
-        .row:after{
-            content:"" ;
+
+        .row:after {
+            content: "";
             display: table;
             clear: both;
         }
-        a{
-          display: block;
-          padding: 10px;
-          width: 100%;
-          text-decoration: none;
+
+        a {
+            display: block;
+            padding: 10px;
+            width: 100%;
+            text-decoration: none;
 
         }
-        a:hover{
+
+        a:hover {
             background-color: black;
             color: white;
         }
     </style>
 </head>
+
 <body>
     <div class="row">
         <div class="column left">
             <h2>Column 1</h2>
             <p>Some text...</p>
+            <a href="?page=glowna">Strona <i>Główna</i></a>
+            <!-- <a href="">link 1</a>
             <a href="">link 1</a>
             <a href="">link 1</a>
             <a href="">link 1</a>
             <a href="">link 1</a>
             <a href="">link 1</a>
-            <a href="">link 1</a>
-            <a href="">link 1</a>
-            <a href="">link 1</a>
+            <a href="">link 1</a> -->
         </div>
         <div class="column right">
             <h2>Column 2</h2>
             <p>Some text...</p>
+            <?php
+            if (isset($_GET['page']))
+            {
+            if (file_exists('pages/' . $_GET['page'] . '.php'))
+            {
+            include 'pages/' . $_GET['page'] . '.php';
+            }
+            else
+            {
+            echo '<h1>Nie znaleziono żądanej strony</h1>';
+            }
+            }
+            else
+            {
+            include 'pages/glowna.php';
+            }
+            ?>
         </div>
     </div>
 </body>
+
 </html>
