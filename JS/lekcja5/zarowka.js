a0915ac1bb5fdd992c105a4395aaf833c3a838be
turtlehtml <p.kozlowski.1312@gmail.com>
2025-10-07 11:54:40 +0200

    oj nie wiem jak ale tak bedzie i ****

---
+++ JS/lekcja5/zarowka.js
@@ -0,0 +1,22 @@
+const zaruwka = document.querySelector('#niezar')
+const guzikon = document.querySelector('#on')
+const guzikoff = document.querySelector('#off')
+
+function pierwsze() {
+	zaruwka.src = 'zdjecia/tak_zarowka.png'
+}
+function drugie() {
+	zaruwka.src = 'zdjecia/nie_zarowka.png'
+}
+
+const one = document.querySelector('#wlacz')
+const nieone = document.querySelector('#wylacz')
+
+one.addEventListener('change', zmiana)
+function zmiana(a) {
+	zaruwka.src = 'zdjecia/tak_zarowka.png'
+}
+nieone.addEventListener('change', zmianaa)
+function zmianaa(e) {
+	zaruwka.src = 'zdjecia/nie_zarowka.png'
+}
