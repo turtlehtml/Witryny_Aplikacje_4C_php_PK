63d243f07d1961777b1f0c95169ae25b3ab450c9
turtlehtml <p.kozlowski.1312@gmail.com>
2025-10-28 15:08:48 +0100

    poprawione bo trzeba poprawiac zeby np juz sie brac

--- JS/lekcja10/kwartaly.js
+++ JS/lekcja10/kwartaly.js
@@ -1,4 +1,29 @@
 let miesiac = Math.floor((Math.random() * 15) + 1)
+let p1 =  document.querySelector('#p1')
+let p2 =  document.querySelector('#p2')
+p1.innerHTML=`${miesiac}`
 switch(miesiac){
-    case
+    case 1:
+    case 2:
+    case 3:
+        p2.innerHTML='I kwartał'
+        break
+    case 4:
+    case 5:
+    case 6:
+        p2.innerHTML='II kwartał'
+        break
+    case 7:
+    case 8:
+    case 9:
+        p2.innerHTML='III kwartał'
+        break
+    case 10:
+    case 11:
+    case 12:
+        p2.innerHTML='IV kwartał'
+        break
+    default:
+        p2.innerHTML='Błędny numer miesiąca'
+        break
 }
