d980ab227448438745ded790ce388aa2262cadf6
turtlehtml <p.kozlowski.1312@gmail.com>
2025-11-18 12:09:41 +0100

    nieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- JS/lekcja14/zegar.js
+++ JS/lekcja14/zegar.js
@@ -4,8 +4,8 @@ const wartosc1 = document.querySelector('#pm')
 const zakres1 = document.querySelector('#minuta')
 let liczenie = (30 * `${wartosc}` - ((11/2) * `${wartosc1}`))
 const wynik = document.querySelector('#wynik')
-let zawartosc = wartosc.value
-let zawartosc1 = wartosc1.value 
+let zawartosc = zakres.value
+let zawartosc1 = zakres1.value 
 
 
 wartosc.textContent = zakres.value
@@ -20,5 +20,6 @@ wartosc1.textContent = zakres1.value
 zakres1.addEventListener('input', event => {
 	wartosc1.textContent = event.target.value
     wynik.innerText = liczenie
+    console.log(zawartosc1)
 })
 
