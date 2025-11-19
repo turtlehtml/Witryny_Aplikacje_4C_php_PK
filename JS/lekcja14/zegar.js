1323560f34939c4472d44c8a76f36aa0c063af4b
turtlehtml <p.kozlowski.1312@gmail.com>
2025-11-19 08:23:05 +0100

    jej

--- JS/lekcja14/zegar.js
+++ JS/lekcja14/zegar.js
@@ -1,25 +1,25 @@
 const wartosc = document.querySelector('#pg')
-const zakres = document.querySelector('#godzinka')
 const wartosc1 = document.querySelector('#pm')
+const zakres = document.querySelector('#godzinka')
 const zakres1 = document.querySelector('#minuta')
-let liczenie = (30 * `${wartosc}` - ((11/2) * `${wartosc1}`))
 const wynik = document.querySelector('#wynik')
-let zawartosc = zakres.value
-let zawartosc1 = zakres1.value 
-
-
-wartosc.textContent = zakres.value
+wynik.innerHTML = `0 &#176`
+wartosc.innerText = zakres.value
+wartosc1.innerText = zakres1.value
+let liczenie = 30 * `${wartosc.value}` - (11 / 2) * `${wartosc1.value}`
 zakres.addEventListener('input', event => {
-	wartosc.textContent = event.target.value
-    wynik.innerText=liczenie
-    console.log(zawartosc)
+	wartosc.innerText = event.target.value
+    let liczenie = Math.abs(30 * `${wartosc.value}` - (11 / 2) * `${wartosc1.value}`)
+    wynik.innerHTML = liczenie +`&#176;`
+
+})
+zakres1.addEventListener('input', event2 =>{
+    wartosc1.innerText = event2.target.value
+    let liczenie = Math.abs(30 * `${wartosc.value}` - (11 / 2) * `${wartosc1.value}`)
+    wynik.innerHTML = liczenie+`&#176;`
+    
 })
 
 
-wartosc1.textContent = zakres1.value
-zakres1.addEventListener('input', event => {
-	wartosc1.textContent = event.target.value
-    wynik.innerText = liczenie
-    console.log(zawartosc1)
-})
 
+ 
