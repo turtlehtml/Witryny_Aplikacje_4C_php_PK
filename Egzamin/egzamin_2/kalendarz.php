88b30a07b20e98a89c487cfeabbd76481d64b9d8
turtlehtml <p.kozlowski.1312@gmail.com>
2025-11-24 16:14:38 +0100

    jo oj oj oj faze gl rodzina

--- Egzamin/egzamin_2/kalendarz.php
+++ Egzamin/egzamin_2/kalendarz.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalendarz</title>
-    <link rel="stylesheet" href="/Egzamin/egzamin_2/INF.03-03-25.01-SG/styl.css">
+    <link rel="stylesheet" href="/egzamin_2/INF.03-03-25.01-SG/styl.css">
 </head>
 
 <body>
